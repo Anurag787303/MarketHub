bd089d731d0ba79958e2ff51b85116fc861bcf20
Anurag Meena <anurag31415@gmail.com>
2023-10-03 23:35:39 +0530

    handle uncaught and unhandled exceptions

--- server/config/database.js
+++ server/config/database.js
@@ -4,9 +4,6 @@ const connectDatabase = () => {
     mongoose.connect(process.env.DB_LOCAL_URI, {}).then(con => {
         console.log(`MongoDB connected with HOST: ${con.connection.host}`)
     })
-        .catch(err => {
-            console.log(err)
-        })
 }
 
 module.exports = connectDatabase
--- server/server.js
+++ server/server.js
@@ -2,12 +2,28 @@ const app = require('./app')
 const dotenv = require('dotenv')
 const connectDatabase = require('./config/database')
 
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exception`);
+})
+
 //Config
 dotenv.config({ path: 'server/config/config.env' })
 
 // Connect to database
 connectDatabase()
 
-app.listen(process.env.PORT || 3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is working on http://localhost:${process.env.PORT || 3000}`)
+})
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", err => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+    server.close(() => {
+        process.exit(1);
+    });
 })
